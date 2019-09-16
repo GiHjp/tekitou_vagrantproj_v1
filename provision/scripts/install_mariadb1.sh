ff496ed59004a75f5913684c2f3002bb5fca2a26
prrn <54540206+prrn-pg@users.noreply.github.com>
2019-09-16 13:41:34 +0900

    🍺

--- provision/scripts/install_mariadb1.sh
+++ provision/scripts/install_mariadb1.sh
@@ -1,4 +1,5 @@
 echo "----- 💪the turn of mariaDB🤳-----"
-sudo yum remove mysql*
+sudo yum -y remove mysql*
+sudo yum -y remove MariaDB*
 sudo rpm --import https://yum.mariadb.org/RPM-GPG-KEY-MariaDB/etc/yum.repos.d/mariadb.repo
 echo "----- next, copy the repo -----"
