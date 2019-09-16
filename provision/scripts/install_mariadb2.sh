bd455e581918230f235e28123b87d3643b1f5304
prrn <54540206+prrn-pg@users.noreply.github.com>
2019-09-16 13:43:29 +0900

    🍜

--- provision/scripts/install_mariadb2.sh
+++ provision/scripts/install_mariadb2.sh
@@ -2,5 +2,5 @@ echo "----- move repos and intall phpmyadmin -----"
 sudo mv ./mariadb.repo /etc/yum.repos.d
 sudo yum -y install MariaDB-devel MariaDB-client MariaDB-server
 echo "----- 🍺chill out🍺 -----"
-sudo systemctl start mysqld
-sudo systemctl enable mysqld
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
