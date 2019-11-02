8843b1e4844460113ed18698e839c87645da2afe
halllllll <h4l@yahoo.ne.jp>
2019-11-02 23:33:51 +0900

    🎃

--- provision/scripts/install_php.sh
+++ provision/scripts/install_php.sh
@@ -6,5 +6,5 @@ sudo yum install -y http://rpms.remirepo.net/enterprise/remi-release-73.rpm
 sudo yum remove php-*
 echo "----- let's install!! here we go... -----"
 sudo yum-config-manager --enable remi-php73
-sudo yum install -y php php-common php-opcache php-mcrypt php-cli php-gd php-curl php-mysqlnd php-intl php-xmlwriter
+sudo yum install -y php php-common php-opcache php-mcrypt php-cli php-gd php-curl php-zip php-devel php-mbstring php-xml php-pear php-json php-pdo php-pecl-apcu php-pecl-apcu-devel php-bcmatch php-imap php-ldap php-odbc php-xmlrpc php-soap php-tidy
 echo "----- 🍺it's all over🍺 -----"
